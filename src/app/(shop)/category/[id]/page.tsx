cedd2ff5ffe75b9d6bab2f29a07e49128396850d
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-08-08 20:10:12 -0500

    fix(category): await async params before use

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,4 +1,17 @@
-export default function CategoryPage() {
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function CategoryPage({ params }: Props) {
+  const { id } = await params;
+  if (id === "kids") {
+    notFound();
+  }
+
   return (
     <div>
       <h1>Category Page</h1>
