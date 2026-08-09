189849446af95f07ca2e95e80f2bd2dc4a5197e1
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-08-09 02:04:24 -0500

    feat(top-menu): open sidebar on menu click with transition

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { titleFont } from "@/config/fonts";
 import Link from "next/link";
 import { IoSearchOutline, IoCartOutline } from "react-icons/io5";
+import { useUIStore } from "@/store";
 
 export const TopMenu = () => {
+  const openSideMenu = useUIStore((state) => state.openSideMenu);
+
   return (
     <nav className="flex px-5 justify-between items-center w-full">
       {/* Logo */}
@@ -48,7 +53,10 @@ export const TopMenu = () => {
             <IoCartOutline className="w-5 h-5" />
           </div>
         </Link>
-        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+        <button
+          onClick={openSideMenu}
+          className="m-2 p-2 rounded-md transition-all duration-200 hover:bg-gray-100 active:scale-95"
+        >
           Menu
         </button>
       </div>
