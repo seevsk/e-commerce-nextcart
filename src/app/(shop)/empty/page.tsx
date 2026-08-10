45d0102e679478c5b8331069b4bd87cf7a034827
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-08-10 06:54:23 -0500

    feat(shop): add empty cart page

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5";
+
 export default function EmptyPage() {
   return (
-    <div>
-      <h1>EmptyPage Page</h1>
+    <div className="flex justify-center items-center h-200">
+      <IoCartOutline size={80} className="mx-5" />
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-semibold">Your cart is empty</h1>
+        <Link className="text-blue-500 mt-2 text-4xl" href={"/"}>
+          Back
+        </Link>
+      </div>
     </div>
   );
 }
