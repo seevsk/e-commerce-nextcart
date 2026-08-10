8b875cfb623148659da77f556014f90dc8455a52
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-08-10 06:53:52 -0500

    style(shop): add mobile padding to store page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -5,9 +5,9 @@ export const products = initialData.products;
 
 export default function Home() {
   return (
-    <>
+    <div className="px-4 sm:px-0">
       <Title title="Store" subtitle="All Products" className="mb-2" />
       <ProductGrid products={products} />
-    </>
+    </div>
   );
 }
