8de95dac120fee340e7651dfb556938e6f21443b
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-08-11 05:43:40 -0500

    feat(db): add database seed script

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,62 @@
+import "dotenv/config";
+import prisma from "../lib/prisma";
+import { initialData } from "./seed";
+
+async function main() {
+  // 1. Delete previous records
+  //   await Promise.all([
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+  //   ]);
+
+  const { categories, products } = initialData;
+
+  // Categories
+  const categoriesData = categories.map((name) => ({ name }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce(
+    (map, category) => {
+      map[category.name.toLowerCase()] = category.id;
+      return map;
+    },
+    {} as Record<string, string>,
+  );
+
+  // Products
+  await Promise.all(
+    products.map(async (product) => {
+      const { type, images, ...rest } = product;
+      const dbProduct = await prisma.product.create({
+        data: {
+          ...rest,
+          categoryId: categoriesMap[type],
+        },
+      });
+
+      // Images
+      const imagesData = images.map((image) => ({
+        url: image,
+        productId: dbProduct.id,
+      }));
+
+      await prisma.productImage.createMany({
+        data: imagesData,
+      });
+    }),
+  );
+
+  console.log("Seed Executed");
+}
+
+(() => {
+  if (process.env.NODE_ENV === "production") return;
+
+  main();
+})();
