3594b230eedd34858a892ed5b021c3a996417b3f
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-08-12 02:27:11 -0500

    feat(product): add paginated products server action

---
+++ src/actions/product/product-pagination.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+
+  try {
+    // Get the products
+    const products = await prisma.product.findMany({
+      take: take,
+      skip: (page - 1) * take,
+      include: {
+        productImages: {
+          take: 2,
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+
+    // Get total pages
+    // todo:
+    const totalCount = await prisma.product.count({});
+    const totalPages = Math.ceil(totalCount / take);
+
+    return {
+      currentPage: page,
+      totalPages: totalPages,
+      products: products.map((product) => ({
+        ...product,
+        images: product.productImages.map((image) => image.url),
+      })),
+    };
+  } catch {
+    throw new Error("The products were not uploaded");
+  }
+};
